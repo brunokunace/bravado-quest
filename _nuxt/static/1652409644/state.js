82ca819ff666b3722cb743083eddd900b18a31b2
brunokunace <brunokunace@users.noreply.github.com>
2022-05-13 03:04:33 +0000

    deploy: e2408ce6a42734aa69b25624713053b06177fa54

--- _nuxt/static/1652409644/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbravado-quest\u002F_nuxt\u002Fstatic\u002F1652409644",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fbravado-quest\u002F",assetsPath:"\u002Fbravado-quest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
