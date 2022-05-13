71641ca4d8fc8731e61bedf7417142885d955fa6
Bruno Henry <brunohenryt@hotmail.com>
2022-05-12 23:35:00 -0300

    chore: deploy to github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: process.env.NODE_ENV === 'production' ? '/bravado-quest/' : '/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
